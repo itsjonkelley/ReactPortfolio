21d3eb434038db9ea2bbc855e3f3ad94a52ea2da
Jonathan Kelley <jonathankelley87@gmail.com>
2020-07-25 23:59:19 -0400

    all pages are rendering

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import about from './containers/about';
+import projects from './containers/projects';
+import contact from "./containers/contact";
+import './style.css';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <Route exact path="/" component={about}/>
+      <Route exact path="/about" component={about}/>
+      <Route exact path="/projects" component={projects}/>
+      <Route exact path="/contact" component={contact}/>
+    </Router>
   );
 }
 
